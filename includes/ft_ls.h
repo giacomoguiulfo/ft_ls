f8647c7b4c365a6aa011d801b968b2ae7d2d59f9
gguiulfo <gguiulfo@student.42.fr>
2017-04-25 07:43:57 -0700

    header file updated

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,19 +6,27 @@
 /*   By: gguiulfo <gguiulfo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 15:01:04 by gguiulfo          #+#    #+#             */
-/*   Updated: 2017/04/24 01:45:49 by gguiulfo         ###   ########.fr       */
+/*   Updated: 2017/04/25 07:00:25 by gguiulfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include <libft.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <pwd.h>
-#include <grp.h>
-#include <uuid/uuid.h>
-#include <dirent.h>
+# include <libft.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <grp.h>
+# include <uuid/uuid.h>
+# include <dirent.h>
+# include <errno.h>
+
+typedef struct	s_file
+{
+	char 		*name;
+	char 		*path;
+	struct stat	statbuf;
+}				t_file;
 
 #endif
